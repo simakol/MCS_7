37dd69b5ac0742f91e931167ca7179aa5e93e64b
Alexey Simak <>
2025-01-23 18:43:43 +0200

    :sparkles: lesson-06

---
+++ js/02-methods.js
@@ -0,0 +1,36 @@
+/*
+ * Об'єкти
+ *
+ * - Методи об'єкта
+ * - Доступ до властивостей об'єкта через this
+ */
+
+/*
+this - контекст виклику фукнції
+- це ключове слово яке зберігає в собі посилання на обʼєкт, який викликає данну фукнцію. Значення this отримується тільки під час виклику.
+
+Обʼєкт який викликає фукнцію це обʼєкт який стоїть до крапки під час виклику методу object.method() - object це обʼєкт, який викликав функцію (метод) method(), тому всередині цієї функції this буде посилатись на обʼєкт object.
+*/
+
+const playlist = {
+  name: 'My amazing playlist',
+  rating: 5,
+  tracks: ['track-1', 'track-2', 'track-3'],
+  getTracksCount() {
+
+  },
+  updateRating(newRating) {
+
+  },
+  changeName(newName) {
+
+  },
+};
+
+playlist.updateRating(3);
+
+playlist.changeName('New playlist');
+
+console.log(playlist);
+
+console.log(playlist.getTracksCount());
